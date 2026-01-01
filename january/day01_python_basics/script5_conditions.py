ce39f40ee71823b72add53f9f4971d9e4f32c54a
Aditya Bhardwaj <advaitonly@gmail.com>
2026-01-01 23:59:08 +0530

    Add salary evaluation conditions

---
+++ january/day01_python_basics/script5_conditions.py
@@ -0,0 +1,9 @@
+salary = int(input("Enter your salary"))
+
+if salary >= 80000:
+  print("Excellent Expectation")
+elif salary >= 50000:
+  print("Good Expectation")
+else:
+  print("Needs Improvement")
+
